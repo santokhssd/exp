fc84b21d4bdb901f5205e0fbe8e3dce0bf486eec
santokhssd <mit.6195@gmail.com>
2017-05-15 19:54:54 +0530

    testing jenkins

---
+++ Square.java
@@ -0,0 +1,26 @@
+class Square{
+	
+	public static void main(String args[])
+	{
+		String s="";
+		for(int i=0;i<7;i++)
+		{
+			s=s+"*";
+		}
+		System.out.println(s); s="*";
+		for(int i=0;i<5;i++)
+		{
+			s=s+" ";
+		}s=s+"*";
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(s);
+		} s="";
+		for(int i=0;i<7;i++)
+		{
+			s=s+"*";
+		}
+		System.out.println(s);
+	}
+}
